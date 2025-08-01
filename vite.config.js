b2445cb41663c84813871703d5115914e6fdc772
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2025-08-02 06:29:17 +0700

    feat: Configure GitHub Pages deployment and update base URL in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	assetsInclude: ["**/*.JPG", "**/*.JPEG", "**/*.PNG", "**/*.WEBP"],
+	base: process.env.NODE_ENV === 'production' ? '/SuLongHoi-platform/' : '/',
 });
